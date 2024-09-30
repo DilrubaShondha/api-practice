1f568a167cf42109c4227158463353db39eddf17
DilrubaShondha <dilrubaeasmin2867@gmail.com>
2024-10-01 00:10:08 +0600

    update commit

--- post.js
+++ post.js
@@ -6,10 +6,12 @@ function loadPost(){
 function displayPosts(posts){
 const container = document.getElementById('post-container');
 for(const post of posts){
-const div = document.createElement('div');
-div.innerHTML = 
+const postDiv = document.createElement('div');
+postDiv.innerHTML = `
  <h4>User-${post.userId}</h4>
- <h5>Post:title</h5>
-}}
-container.appendChild(div)
+ <h5>Post:title ${post.title}</h5>
+ <p>Post Description: ${post.body}</p>`
+ container.appendChild(postDiv)
+}
+}
 loadPost();
