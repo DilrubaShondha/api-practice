0d7d545b1350f33b790e586584ade4c85e8cce4c
DilrubaShondha <dilrubaeasmin2867@gmail.com>
2024-10-01 00:00:15 +0600

    second commit

--- post.js
+++ post.js
@@ -1,5 +1,15 @@
-// fetch('https://jsonplaceholder.typicode.com/posts')
 function loadPost(){
-    console.log("loading post .........")
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then (res => res.json())
+    .then (data => displayPosts(data))
 }
+function displayPosts(posts){
+const container = document.getElementById('post-container');
+for(const post of posts){
+const div = document.createElement('div');
+div.innerHTML = 
+ <h4>User-${post.userId}</h4>
+ <h5>Post:title</h5>
+}}
+container.appendChild(div)
 loadPost();
